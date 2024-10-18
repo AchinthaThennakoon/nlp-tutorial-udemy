edd396368b256b256a80b1ddbc78ef74cd2c2dfd
Achintha Thennakoon <achinthathennakoon97@gmail.com>
2024-10-18 22:34:05 +0530

    remove comments

--- Classification(Naive-Bayes)/classification.py
+++ Classification(Naive-Bayes)/classification.py
@@ -4,18 +4,12 @@ import random
 
 documents = []
 
-# print(movie_reviews.categories())
-#
-# # Displays frequency of words in ‘movie_reviews’
-# print(nltk.FreqDist(movie_reviews.words()))
-
 for category in movie_reviews.categories():
     for fileid in movie_reviews.fileids(category):
         review_word_list = list(movie_reviews.words(fileids=fileid))
         document = (review_word_list, category)
         documents.append(document)
 
-# print(documents[0])
 
 random.shuffle(documents)
 
